87e62729fdd6612c5f920c7269319c1baf4510df
Philip Bove <philip.bove@uspsector.com>
2018-10-26 17:52:13 -0400

    working on untitests

--- lib/ansible/module_utils/net_tools/tenable.py
+++ lib/ansible/module_utils/net_tools/tenable.py
@@ -3,117 +3,120 @@ from six.moves import urllib
 from ansible.module_utils.urls import Request
 
 class TenableAPI:
-	ignore_params = ['username', 'password','validate_certs','state','server']
+    ignore_params = ['username', 'password','validate_certs','state','server']
 
-	def __init__(self, module):
-		self.session = Request(validate_certs=module.params['validate_certs'],headers={'Content-Type':'application/json'})
-		self.module = module
+    def __init__(self, module):
+        self.session = Request(validate_certs=module.params['validate_certs'],headers={'Content-Type':'application/json'})
+        self.module = module
 
 
-	def login(self):
-		try:
-			response = self.session.post(self.module.params['server']+'/rest/token', data=(json.dumps({'username':self.module.params['username'],
-				'password':self.module.params['password']})))
-			self.session.headers['X-SecurityCenter'] = str(json.loads(response.read())['response']['token'])
-		except urllib.error.HTTPError as error:
-			self.__handle_http_error__(error)
-	
-	def exists(self,item,data):
-		if self.get_item_by_name(item, data['name']) != None:
-			return True
-		return False
+    def login(self):
+        try:
+            response = self.session.post(self.module.params['server']+'/rest/token', data=(json.dumps({'username':self.module.params['username'],
+                'password':self.module.params['password']})))
+            self.session.headers['X-SecurityCenter'] = str(json.loads(response.read())['response']['token'])
+        except urllib.error.HTTPError as error:
+            self.__handle_http_error__(error)
+    
+    def exists(self,item,data):
+        if self.get_item_by_name(item, data['name']) != None:
+            return True
+        return False
 
-	def create(self, item, data):
-		try:
-			response = self.session.post(self.module.params['server']+'/rest/'+item,data=json.dumps(data))
-		except urllib.error.HTTPError as error:
-			self.__handle_http_error__(error)
+    def create(self, item, data):
+        try:
+            response = self.session.post(self.module.params['server']+'/rest/'+item,data=json.dumps(data))
+        except urllib.error.HTTPError as error:
+            self.__handle_http_error__(error)
 
-	# recursivley walk the dictonary checking if data is the same
-	def is_different(self,data, existing_data):
-		difference = False
-		for item in data.keys():
-			# if dict pass onto function again
-			if type(data[item]) == dict:
-				difference = self.is_different(data[item], existing_data[item])
-			# if list, loop through that and pass onto function
-			elif type(data[item]) == list:
-				if len(existing_data[item]) == 0:
-					difference = True
-					break
-				for dictionary in data[item]:
-					for dicts in existing_data[item]:
-						if dictionary['id'] == ['id']:
-							id_dict = dicts
-							break
-						id_dict = None
-					if id_dict:
-						difference = self.is_different(dictionary,
-							id_dict)
-			# this evaluates the items at the end
-			elif str(data[item]) != existing_data[item]:
-				difference = True
-				break
-		return difference
+    # recursivley walk the dictonary checking if data is the same
+    def is_different(self,data, existing_data):
+        difference = False
+        for item in data.keys():
+            # if dict pass onto function again
+            if item not in existing_data.keys():
+                return True
+            elif type(data[item]) == dict:
+                difference = self.is_different(data[item], existing_data[item])
+            # if list, loop through that and pass onto function
+            elif type(data[item]) == list:
+                if len(existing_data[item]) == 0:
+                    difference = True
+                    break
+                for dictionary in data[item]:
+                    for dicts in existing_data[item]:
+                        if str(dictionary['id']) == dicts['id']:
+                            id_dict = dicts
+                            break
+                        id_dict = None
+                    if id_dict:
+                        difference = self.is_different(dictionary,
+                            id_dict)
+            # this evaluates the items at the end
+            elif str(data[item]) != existing_data[item]:
+                difference = True
+                break
+        print(difference)
+        return difference
 
-	# this method updates the item you are working on
-	# but only if it is different
-	def update(self, item, data, existing_data):
-		try:
-			if self.is_different(data, existing_data):
-				response = self.session.patch(self.module.params['server']+'/rest/'+item+'/'+existing_data['id'],data=json.dumps(data))
-				return True
-			else:
-				return False
-		except urllib.error.HTTPError as error:
-			self.__handle_http_error__(error)
+    # this method updates the item you are working on
+    # but only if it is different
+    def update(self, item, data, existing_data):
+        try:
+            if self.is_different(data, existing_data):
+                response = self.session.patch(self.module.params['server']+'/rest/'+item+'/'+existing_data['id'],data=json.dumps(data))
+                return True
+            else:
+                return False
+        except urllib.error.HTTPError as error:
+            self.__handle_http_error__(error)
 
-	# only return data for use that is not the alias or the specifcally ignored paramaters
-	# that are the same across all the modules
-	def clean_data(self):
-		arg_spec = [x for x in self.module.argument_spec.keys() if x not in self.ignore_params]
-		return {key: value for key, value in self.module.params.items() if key in arg_spec and value}
+    # only return data for use that is not the alias or the specifcally ignored paramaters
+    # that are the same across all the modules
+    def clean_data(self):
+        arg_spec = [x for x in self.module.argument_spec.keys() if x not in self.ignore_params]
+        return {key: value for key, value in self.module.params.items() if key in arg_spec and value}
 
-	def remove(self, item, data, existing_data):
-		try:
-			response =  self.session.delete(self.module.params['server']+'/rest/'+item+'/'+existing_data['id'])
-			return True
-		except urllib.error.HTTPError as error:
-			self.__handle_http_error__(error)
+    def remove(self, item, data, existing_data):
+        try:
+            response =  self.session.delete(self.module.params['server']+'/rest/'+item+'/'+existing_data['id'])
+            return True
+        except urllib.error.HTTPError as error:
+            self.__handle_http_error__(error)
 
-	# translates item by name to id
-	def get_item_by_name(self, item, name,objtype='manageable'):
-		try:
-			response = self.session.get(self.module.params['server']+'/rest/'+item)
-			response_json = json.loads(response.read())
-			if type(response_json['response']) == dict:
-				for obj in response_json['response'][objtype]:
-					if obj['name'] == name:
-						response = self.session.get(self.module.params['server']+'/rest/'+item+'/'+obj['id'])
-						return json.loads(response.read())['response']
-			else:
-				for obj in response_json['response']:
-					if obj['name'] == name:
-						response = self.session.get(self.module.params['server']+'/rest/'+item+'/'+obj['id'])
-						return json.loads(response.read())['response']
-			return None
-		except urllib.error.HTTPError as error:
-			self.__handle_http_error__(error)
+    # translates item by name to id
+    def get_item_by_name(self, item, name,objtype='manageable'):
+        try:
+            response = self.session.get(self.module.params['server']+'/rest/'+item)
+            response_json = json.loads(response.read())
+            if type(response_json['response']) == dict:
+                for obj in response_json['response'][objtype]:
+                    if obj['name'] == name:
+                        response = self.session.get(self.module.params['server']+'/rest/'+item+'/'+obj['id'])
+                        return json.loads(response.read())['response']
+            else:
+                for obj in response_json['response']:
+                    if obj['name'] == name:
+                        response = self.session.get(self.module.params['server']+'/rest/'+item+'/'+obj['id'])
+                        return json.loads(response.read())['response']
+            return None
+        except urllib.error.HTTPError as error:
+            self.__handle_http_error__(error)
 
-	def build_id_fields(self,id_maps, params,objtype='usable'):
-		for item in id_maps.keys():
-			if item in params.keys():
-				if self.module.argument_spec[item]['type'] == list:
-					id_param_list = []
-					for name in params[item]:
-						id_param_list.append(self.__build_id_field__(id_maps[item]['endpoint'],name,objtype))
-						params[item] = id_param_list 
-				else:
-					params[item] = self.__build_id_field__(id_maps[item]['endpoint'],params[item],objtype)
+    def build_id_fields(self,id_maps, params,objtype='usable'):
+        for item in id_maps.keys():
+            if item in params.keys():
+                if self.module.argument_spec[item]['type'] == list:
+                    id_param_list = []
+                    for name in params[item]:
+                        id_param_list.append(self.__build_id_field__(id_maps[item]['endpoint'],name,objtype))
+                        params[item] = id_param_list 
+                else:
+                    params[item] = self.__build_id_field__(id_maps[item]['endpoint'],params[item],objtype)
 
-	# helper method that subs a user paramter using a name with the id for the api call
-	def __build_id_field__(self, item, name,objtype='usable'):
-		return {'id': int(self.get_item_by_name(item, name,objtype)['id'])}
+    # helper method that subs a user paramter using a name with the id for the api call
+    def __build_id_field__(self, item, name,objtype='usable'):
+        return {'id': int(self.get_item_by_name(item, name,objtype)['id'])}
 
-	def __handle_http_error__(self,error):
-		self.module.fail_json(msg=str(error.code) +' '+ error.reason) #['error_msg'])
+    def __handle_http_error__(self,error):
+        self.module.fail_json(msg=str(error.code) +' '+ error.reason) #['error_msg'])
--- test/units/module_utils/net_tools/test_tenable.py
+++ test/units/module_utils/net_tools/test_tenable.py
@@ -1,70 +1,94 @@
-from ansible.module_utils.tenable import TenableAPI
+from ansible.module_utils.net_tools.tenable import TenableAPI
 from units.compat.mock import patch, MagicMock, Mock
 import unittest
+import copy
 
 
-class TestTenable(unittest.TestCase):
+def get_mock_module():
+    module = MagicMock(name='AnsibleModule')
+    module.check_mode = False
+    module.argument_spec = argument_spec = dict(
+        server = dict(type=str, required=True),
+        name = dict(type=str, required=True),
+        username = dict(type=str,required=True),
+        password = dict(type=str, required=True,no_log=True),
+        type = dict(type=str, required=False, choices=['plugin', 'policy']),
+        zone = dict(type=str, required=False),
+        dhcpTracking = dict(type=bool, required=False, aliases=['dhcp_tracking']),
+        classifyMitigatedAge = dict(type=int, required=False, default=0, 
+            aliases=['classify_mitagated_age']),
+        schedule = dict(type=dict, required=False,
+            default={'type':'template'}),
+        reports = dict(type=list, required=False),
+        repository = dict(type=str,required=False),
+        assets = dict(type=list, required=False),
+        credentials = dict(type=list, required=False),
+        emailOnLaunch = dict(type=bool, required=False, default=False, choices=[True, False],
+            aliases=['email_on_launch']),
+        emailOnFinish = dict(type=bool, required=False, default=False, choices=[True, False],
+            aliases=['email_on_finish']),
+        timeoutAction = dict(type=str, required=False, default='import', 
+        choices=['import','discard','import'], aliases=['timeout_action']),
+        scanningVirtualHosts = dict(type=bool, required=False, default=False, choices=[True, False],
+            aliases=['scanning_virtual_hosts']),
+        rolloverType = dict(type=str, required=False, default='template', choices=['nextDay','template'],
+            aliases=['rollover_type']),
+        policy = dict(type=str,required=False),
+        ipList = dict(type=list, required=False,aliases=['ip_list']),
+        maxScanTime = dict(type=int, required=False, default=3600, aliases=['max_scan_time']),
+                    validate_certs = dict(type=bool, required=False, default=True, choices=[True,False]),
+                    state = dict(type=str,required=False, default='present',choices=['present','absent'])
+        )
+    module.params= {
+        "assets": None,
+        "classifyMitigatedAge": 0,
+        "credentials": None,
+        "dhcpTracking": None,
+        "emailOnFinish": False,
+        "emailOnLaunch": False,
+        "ipList": [
+            "10.10.27.61",
+            "10.10.27.62"
+        ],
+        "ip_list": [
+            "10.10.27.61",
+            "10.10.27.62"
+        ],
+        "maxScanTime": 3600,
+        "name": "tenable-scan-test-scan",
+        "password": "VALUE_SPECIFIED_IN_NO_LOG_PARAMETER",
+        "policy": "5.4.x non-Windows DIACAP Vulnerability Scan Policy",
+        "reports": None,
+        "repository": "cloud repos high > 10.10.xx.xx",
+        "rolloverType": "template",
+        "scanningVirtualHosts": False,
+        "schedule": {
+            "type": "template"
+        },
+        "server": "https://server/",
+        "state": "present",
+        "timeoutAction": "import",
+        "type": "policy",
+        "username": "user",
+        "validate_certs": False,
+        "zone": None
+    }
+    return module
 
-	def set_up(self):
-		self.module = MagicMock(name='AnsibleModule')
-		self.module.check_mode = False
-		self.module.params= {
-            "assets": None,
-            "classifyMitigatedAge": 0,
-            "credentials": None,
-            "dhcpTracking": None,
-            "emailOnFinish": False,
-            "emailOnLaunch": False,
-            "ipList": [
-                "10.10.27.61",
-                "10.10.27.62"
-            ],
-            "ip_list": [
-                "10.10.27.61",
-                "10.10.27.62"
-            ],
-            "maxScanTime": 3600,
-            "name": "tenable-scan-test-scan",
-            "password": "VALUE_SPECIFIED_IN_NO_LOG_PARAMETER",
-            "policy": "5.4.x non-Windows DIACAP Vulnerability Scan) Policy",
-            "reports": None,
-            "repository": "cloud repos high > 10.10.xx.xx",
-            "rolloverType": "template",
-            "scanningVirtualHosts": False,
-            "schedule": {
-                "type": "template"
-            },
-            "server": "https://server/",
-            "state": "present",
-            "timeoutAction": "import",
-            "type": "policy",
-            "username": "user",
-            "validate_certs": False,
-            "zone": None
-        }
-
-    self.sexisting_data = {
-    "type": "regular",
-    "response": {
+def get_data():
+    return {
         "id": "439",
         "name": "tenable-scan-test-scan",
         "description": "",
         "ipList": "10.10.27.61,10.10.27.62",
         "type": "policy",
-        "dhcpTracking": "false",
         "classifyMitigatedAge": "0",
-        "emailOnLaunch": "false",
-        "emailOnFinish": "false",
         "timeoutAction": "import",
-        "scanningVirtualHosts": "false",
         "rolloverType": "template",
         "status": "0",
         "createdTime": "1540400625",
         "modifiedTime": "1540400625",
         "maxScanTime": "3600",
-        "reports": [],
-        "assets": [],
-        "credentials": [],
         "numDependents": "0",
         "schedule": {
             "id": "409",
@@ -77,20 +101,20 @@ class TestTenable(unittest.TestCase):
         "policy": {
             "id": "1000004",
             "context": "",
-            "name": "5.4.x non-Windows DIACAP Vulnerability Scan) Policy",
+            "name": "5.4.x non-Windows DIACAP Vulnerability Scan Policy",
             "description": "Nessus Policy exported from SecurityCenter",
             "tags": "",
             "owner": {
-                "id": "1",
-                "username": "user",
-                "firstname": "",
-                "lastname": ""
-            },
-            "ownerGroup": {
-                "id": "0",
-                "name": "Full Access",
-                "description": "Full Access group"
-            }
+            "id": "1",
+                    "username": "user",
+                    "firstname": "",
+                    "lastname": ""
+                },
+                "ownerGroup": {
+                    "id": "0",
+                    "name": "Full Access",
+                        "description": "Full Access group"
+                }
         },
         "repository": {
             "id": "2",
@@ -100,69 +124,58 @@ class TestTenable(unittest.TestCase):
         "canUse": "true",
         "canManage": "true",
         "plugin": {
-            "id": -1,
-            "name": "",
-            "description": ""
-        },
-        "zone": {
-            "id": -1,
-            "name": "",
-            "description": ""
-        },
-        "ownerGroup": {
-            "id": "0",
-            "name": "Full Access",
-            "description": "Full Access group"
-        },
-        "creator": {
-            "id": "12",
-            "username": "user",
-            "firstname": "user",
-            "lastname": "user"
-        },
-        "owner": {
-            "id": "12",
-            "username": "user",
-            "firstname": "user",
-            "lastname": "user"
-        }
-    },
-    "error_code": 0,
-    "error_msg": "",
-    "warnings": [],
-    "timestamp": 1540400647
-}
+                "id": -1,
+                "name": "",
+                "description": ""
+            },
+            "ownerGroup": {
+                "id": "0",
+                "name": "Full Access",
+                "description": "Full Access group"
+            },
+            "creator": {
+                "id": "12",
+                "username": "user",
+                "firstname": "user",
+                "lastname": "user"
+            },
+            "owner": {
+                "id": "12",
+                "username": "user",
+                "firstname": "user",
+                    "lastname": "user"
+            }
+    }
 
+class TestTenable(unittest.TestCase):
 
-	def test_is_different(self):
-		tenable = TenableAPI(self.module)
-		self.AssertFalse(Tenable.is_different(self.module.params,self.existing_data))
-		diff_data = existing_data.deep_copy()
-		diff_data['ipList'] = '192.168.1.1'
-		self.AssertTrue(Tenable.is_different(self.module.params,diff_data))
-
-	def test_clean_data(self):
-		tenable = TenableAPI(self.module)
-		self.AssertEqual(TenableAPI.clean_data(), {
+    def test_clean_data(self):
+        module = get_mock_module()
+        existing_data = get_data()
+        tenable = TenableAPI(module)
+        self.assertEqual(tenable.clean_data(), {
             "ipList": [
                 "10.10.27.61",
                 "10.10.27.62"
             ],
-            "ip_list": [
-                "10.10.27.61",
-                "10.10.27.62"
-            ],
-            "maxScanTime": 3600,
-            "name": "tenable-scan-test-scan",
-            "policy": "5.4.x non-Windows DIACAP Vulnerability Scan) Policy",
-            "repository": "cloud repos high > 10.10.xx.xx",
-            "rolloverType": "template",
-            "schedule": {
-                "type": "template"
-            },
-            "state": "present",
-            "timeoutAction": "import",
-            "type": "policy",
+          "maxScanTime": 3600,
+          "name": "tenable-scan-test-scan",
+          "policy": "5.4.x non-Windows DIACAP Vulnerability Scan Policy",
+          "repository": "cloud repos high > 10.10.xx.xx",
+          "rolloverType": "template",
+          "schedule": {
+              "type": "template"
+          },
+          "timeoutAction": "import",
+          "type": "policy",
         })
-
-		
+        
+    def test_is_different(self):
+        module = get_mock_module()
+        existing_data = get_data()
+        tenable = TenableAPI(module)
+        new_params = tenable.clean_data()
+        new_params['ipList'] = ','.join(new_params['ipList'])
+        self.assertFalse(tenable.is_different(new_params,existing_data))
+        existing_data['ipList'] = '192.168.1.1'
+        self.assertTrue(tenable.is_different(new_params,diff_data))
